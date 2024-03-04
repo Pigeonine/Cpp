43a34d25c7cf11170c2aa685961b46721e4e92d4
Pigeonine <hyen1222@naver.com>
2024-03-04 13:30:49 +0900

    2024.03.04

---
+++ daily/2024.02/2024.02.16/24927.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(false)
+
+using namespace std;
+
+int main() {
+    fastio;
+    int n, k,temp, counts = 0;
+    cin >> n >> k;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> temp;
+        while(temp % 2 == 0)
+        {
+            counts++; temp /= 2;
+        }
+    }
+
+
+    if(counts >= n) cout << 1;
+    else cout << 0;
+}
